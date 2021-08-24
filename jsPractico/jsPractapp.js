d2fe5c0a27bc1e38f5ad165ff4a23329fadcaeb5
Christiano0407 <placebo0704@gmail.com>
2021-08-24 09:05:18 -0500

    calc

--- jsPractico/jsPractapp.js
+++ jsPractico/jsPractapp.js
@@ -1,5 +1,55 @@
 console.group("Practice Js / Figure Geometric");
-// Figuras Geométricas
-// Parámetro / área / Radio / Diámetro.
-// Cuadrado:
+// Figuras Geométricas ============== >
+
+//  => Parámetro / área / Radio / Diámetro. =>
+
+// == Cuadrado:
+console.log("Cuadrado");
+const ladoCuadrado = 10;
+
+// = Parametro = >
+cuadradoParametro = () => {
+  return ladoCuadrado * 4;
+};
+// = result =>
+console.log(cuadradoParametro());
+
+// == área == >
+const areaCuadrado = (ladoCuadrado * ladoCuadrado) / 2;
+console.log(`Es el área total del cuadrado: ${areaCuadrado}cm`);
+
+// == Triángulo
+let ladoA = 20;
+let ladoB = 10;
+let ladoC = +ladoB;
+//console.log(ladoC);
+// => Perímetro ==> lado + lado + lado.
+parameter = () => {
+  let param = ladoA + ladoB + ladoC;
+  console.log(`Parameter: ${param}`);
+};
+
+// == Area => Base por altura / 2 =>
+area = () => {
+  let calcArea = (ladoA * ladoB) / 2;
+  console.log(calcArea);
+};
+
+parameter();
+area();
+
+// == Círculo === > Radio y Circunferencia === >
+let radioCir = 4;
+let diametroCir = +radioCir * 2;
+//console.log(diametroCir);
+const piRadio = Math.PI;
+
+// == CIRCUNFERENCIA (CALC) =>
+circPerim = () => {
+  let circunferenciaCir = diametroCir * piRadio;
+  console.log(circunferenciaCir);
+};
+
+circPerim();
+
 console.groupEnd();
