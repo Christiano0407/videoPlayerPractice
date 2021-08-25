71f0be3dc9bf2d555aefc505e7865984cbdee168
Christiano0407 <placebo0704@gmail.com>
2021-08-25 09:00:51 -0500

    button and form

--- porcDescuento/appPorcentaje.js
+++ porcDescuento/appPorcentaje.js
@@ -1,3 +1,11 @@
 console.group("Porcentajes y Descuentos");
+// ================ PRECIO / DESCUENTOS / Porcentajes / Ventas =================
+
+// Primer paso: Definir fórmula
+// <<<<<< ===>  Precio * (100 - %) / 100   <=== >>>>> => % = Descuento .
+
+// Segundo: Implementar la fórmula en javascript.
+// Tercer: Crear las funciones.
+// Integrar HTML a Javascipt.
 
 console.groupEnd();
