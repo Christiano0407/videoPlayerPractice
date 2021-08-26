fe02f36ffd66c369c0e16fe809b66c0238eaa392
Christiano0407 <placebo0704@gmail.com>
2021-08-26 12:18:10 -0500

    part01mediana

--- medianaModaPromedio/appMedia.js
+++ medianaModaPromedio/appMedia.js
@@ -2,8 +2,14 @@ console.group("Media / Mediana 7 Calculator");
 // Calcular la media ============ >
 // ===== > Media >
 // <<<<< Variables >>>>>>
-
+const listaUno = [5, 10, 15, 20, 25];
 // <<<<< Funciones / Eventos >>>>>>
 
+const mitadLista = listaUno.length / 2;
+
+// => módulo => % ===
+
 // <<<<< Ejecutar >>>>>>
+console.log(mitadLista);
+
 console.groupEnd();
