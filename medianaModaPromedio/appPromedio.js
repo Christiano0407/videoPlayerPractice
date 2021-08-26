76a0b4676b1dab7fb0da556e1c619633bb400e2a
Christiano0407 <placebo0704@gmail.com>
2021-08-25 20:44:07 -0500

    section

--- medianaModaPromedio/appPromedio.js
+++ medianaModaPromedio/appPromedio.js
@@ -1,3 +1,21 @@
 console.group("Promedio");
+// ========= Taller 3: promedio, mediana y moda ===========>
+
+/*     e ===== STADÍSTICA  ========*/
+//- Primer paso: definir las fórmulas
+//- Segundo paso: implementar la fórmulas en JavaScript
+//- Tercer paso: crear funciones
+//- Cuarto paso: integrar JS con HTML
+//# curso-practico-javascript
+
+// Promedio ==========>
+// Fórmula: Media aritmética:
+/* Ventana1 + ventana2 + ventana3 / cantidad de ventanas */
+
+// MEDIANA ======>
+// Elemento que esté exactamente en medio de la lista.
+
+// MODA ========>
+// Elemento que más se repite de una lista
 
 console.groupEnd();
