27ced2f6108562069254bf1944c26a01e1c41984
Christiano0407 <placebo0704@gmail.com>
2021-08-27 12:11:45 -0500

    change-scroll

--- navScroll/appNavScroll.js
+++ navScroll/appNavScroll.js
@@ -2,31 +2,47 @@ console.group("NavScroll");
 // <<< Variables >>>
 const body = document.body;
 //const scrollUp = document.querySelector(`.scroll-up`);
-
 let lastScroll = 0;
 
-// <<< functions and events >>>
 window.addEventListener(`scroll`, () => {
-  const currentScroll = window.pageXOffset; // pageoffset = scrollY or X =
+  //console.log(window.pageYOffset);
+  const currentScroll = window.pageYOffset;
 
   if (currentScroll <= 0) {
     body.classList.remove("scroll-up");
-    return;
   }
 
   if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
     body.classList.remove("scroll-up");
     body.classList.add("scroll-down");
-  } else if (
-    currentScroll < lastScroll &&
-    body.classList.contains("scroll-down")
-  ) {
-    body.classList.remove(`scroll-down`);
-    body.classList.add(`scroll-up`);
   }
+  // == 0 a conforme bajas / scroll ===
   lastScroll = currentScroll;
 });
 
-// <<< Llamado a function / ejecutar >>>
-
+// =================================================================================== >
+/* // <<< functions and events >>> */
+/* window.addEventListener(`scroll`, () => { */
+/*   const currentScroll = window.pageXOffset; // pageoffset = scrollY or X = */
+/*  */
+/*   if (currentScroll <= 0) { */
+/*     body.classList.remove("scroll-up"); */
+/*     return; */
+/*   } */
+/*  */
+/*   if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) { */
+/*     body.classList.remove("scroll-up"); */
+/*     body.classList.add("scroll-down"); */
+/*   } else if ( */
+/*     currentScroll < lastScroll && */
+/*     body.classList.contains("scroll-down") */
+/*   ) { */
+/*     body.classList.remove(`scroll-down`); */
+/*     body.classList.add(`scroll-up`); */
+/*   } */
+/*   lastScroll = currentScroll; */
+/* }); */
+/*  */
+/* // <<< Llamado a function / ejecutar >>> */
+/*  */
 console.groupEnd();
