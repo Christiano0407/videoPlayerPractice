5ab8687fc06be7b384b597753662887099b17576
Christiano0407 <placebo0704@gmail.com>
2021-08-30 11:56:55 -0500

    salaryTop10

--- analisisSalarios/appAnalisis.js
+++ analisisSalarios/appAnalisis.js
@@ -30,8 +30,10 @@ const analSalaryUSASorted = analSalaryUSA.sort(function (salaryA, salaryB) {
 // 2)
 // < =============== Calcular la mediana ============= >
 
-// >> Saber si el numero es par ===== >>
+// <<<<< Saber si el numero es par o impar ===== >>>>>
 // >>>>>>>>>>>>> Helpers o utils >>>>>>>>>>>>
+// >>>>>>>>>>>>> Si el residuo de la divisió0n %2 = 0 > true (par)  >>>>>>>>>>>>
+// >>>>>>>>>>>>> Si el residuo de la divisió0n %2 != 0 > false (no es par)  >>>>>>>>>>>>
 esPar = (person) => {
   /* if(person % 2 === 0 ) { */
   /*     return true;  */
@@ -42,7 +44,8 @@ esPar = (person) => {
   return person % 2 === 0;
 };
 
-// ===== Calcular la mediana / Function ======>>>>>>
+// ===== Calcular la mediana function / General > cualquier País ======>>>>>>
+// Ayuda al promedio para calcular la media entre medianaSalary / espar o impar
 calcularMediaAritmetica = (list) => {
   const sumList = list.reduce(function (valorAcumulado = 0, nuevoValor) {
     return valorAcumulado + nuevoValor;
@@ -54,6 +57,8 @@ calcularMediaAritmetica = (list) => {
 };
 
 // ===== Promedio ==> Ayudar a calcularlo para la mediana.
+// => ParseInt => Quitar los decimales
+// => -1 => Tenemos que ir a la posición anterior
 medianaSalary = (list) => {
   const middle = parseInt(list.length / 2);
 
@@ -61,6 +66,7 @@ medianaSalary = (list) => {
     const middlePerson1 = list[middle - 1];
     const middlePerson2 = list[middle];
 
+    // > Calcular la mediana de salarios <=
     const mediana = calcularMediaAritmetica([middlePerson1, middlePerson2]);
     return mediana;
   } else {
@@ -72,13 +78,25 @@ medianaSalary = (list) => {
 
 const mediaGeneralUSA = medianaSalary(analSalaryUSASorted);
 
-// ====== Calculate Mediana Top 10% ===== splice() ======= >
+// 3)
+// ====== Calculate Mediana Top 10% = Mayor ingreso ===== splice() ======= >
+// >>> Nuevo método => splice => star (comienza el corte) /count (cuántos cortar) <<<
+const spliceStart = (analSalaryUSASorted.length * 90) / 100; // > Calcular el 10% <
+const spliceCount = analSalaryUSASorted.length - spliceStart; // > Desde dónde cortamos <
+// TOP 10%
+const salaryUSATop10 = analSalaryUSASorted.splice(spliceStart, spliceCount);
+// Llamar el 10%
+const mediaUSATop = medianaSalary(salaryUSATop10);
 
 // === Llamar a las funciones y eventos == >
-//1=>
+//1 = >
 console.log(analSalaryUSA);
 console.log(analSalaryUSASorted);
-//2)=>
+//2) = >
 console.log(esPar());
 console.log(`Es la media o mediana ${mediaGeneralUSA}`);
+// 3) = >
+console.log(salaryUSATop10);
+console.log(`La media del top 10% es ${mediaUSATop}`);
+
 console.groupEnd();
