c47e059cb300cfb68c1020de3994040dec03a773
Christiano0407 <placebo0704@gmail.com>
2021-08-31 21:03:20 -0500

    encapsulamiento

--- objectOP/oppEncapsulamiento.js
+++ objectOP/oppEncapsulamiento.js
@@ -1,3 +1,4 @@
 console.group("Encapsulamiento");
+// Encapsulamiento == > a encapsulación es el empaquetamiento de datos y funciones en un componente (por ejemplo, una clase) y para luego controlar el acceso a ese componente para hacer un ejecto de “caja negra” fuera del objeto. Debido a esto, un usuario de esa clase solo necesita conocer su interfaz (es decir, los datos y las funciones expuestas fuera de la clase), no la implementación oculta.=>
 
 console.groupEnd();
