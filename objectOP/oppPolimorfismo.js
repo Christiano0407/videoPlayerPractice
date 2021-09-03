83fcf7ec90e2143c327d81748ca596931c6fe4e3
Christiano0407 <placebo0704@gmail.com>
2021-09-03 12:33:08 -0500

    polim

--- objectOP/oppPolimorfismo.js
+++ objectOP/oppPolimorfismo.js
@@ -1,3 +1,5 @@
 console.group("OPP Polimorfismo");
-
+// > Casi la herencia 2.0 <
+// Cambiar o anular lo métodos que adquirimos de una class mother (clase madre) >>>>>>>>>>>>>>>>
+// Tipos: Sobrecarga / Paramétrico / Inclusión.
 console.groupEnd();
