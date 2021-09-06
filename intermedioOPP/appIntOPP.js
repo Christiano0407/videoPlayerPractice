08519af7e3e1d6c65b72baa678a8823dbf1df5f5
Christiano0407 <placebo0704@gmail.com>
2021-09-06 09:31:23 -0500

    object2

--- intermedioOPP/appIntOPP.js
+++ intermedioOPP/appIntOPP.js
@@ -11,6 +11,64 @@ console.group("Intermedio Programación Orientada Objetos");
 // Palabra reservada: "getOwnPropertyDescriptors".
 
 // << =============================== Variables ======================= >>
+// 1) STATIC >
+// Atributos > estáticos >>
+class Patito {
+  // método = sonido>
+  static sonido = "Cuak!";
+}
+// Acceder al atributo sonido >
+console.log(Patito.sonido);
+
+//2) static and method
+class Perro {
+  static SonidoDog() {
+    return "Woow!";
+  }
+}
+console.log(Perro.SonidoDog());
+
+// Prototype OBJECT / Instancia de objecto Madre >
+// Objeto completo / array >
+//3)
+const objetito = {
+  name: "Alma",
+  email: "Al@gmail.com",
+  age: 31,
+};
+// > Nos devuelve una lista (array) con todos nuestros objetos >
+// Nombres clave para cada valor>
+console.log(Object.keys(objetito));
+
+//4) getOwnPropertyNames
+// Devuelve un array con los valosres clave >
+const sportWomen = {
+  name: "Alma Rosa",
+  email: "ros@gmail.com",
+  age: 29,
+};
+console.log(Object.getOwnPropertyNames(sportWomen));
+
+//5) entries
+// devuelve un array de arrays (más arrays)
+// Divide en arrays
+const playMen = {
+  name: "Lord89",
+  email: "89@gmail.com",
+  age: 21,
+};
+console.log(Object.entries(playMen));
+
+// 6) getOwnPropertyDescriptors
+// Devuelve todas las propiedades de nuestro objeto >
+// value / writable / configurable / enumerable > Atributos
+// Ayuda a limitar el acceso >
+// Propiedad del Objeto >>
+const marioBros = {
+  name: "Mario Bros",
+  game: "Mario Nintendo",
+};
+console.log(Object.getOwnPropertyDescriptors(marioBros));
 
 // << ================= > functions and events
 
---
+++ intermedioOPP/staticObjectOPPTwo.js
@@ -0,0 +1,5 @@
+console.group("Métodos Estáticos del Prototipo Object / Objeto");
+// <<<< Métodos Estáticos del Prototipo Object / Objeto >>>>
+// > Super Protoype Object.
+
+console.groupEnd();
