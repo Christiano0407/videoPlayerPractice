05cedc09c92abec647972e0eed682cf0c14e72c4
Christiano0407 <placebo0704@gmail.com>
2021-09-06 12:42:21 -0500

    mem

---
+++ intermedioOPP/memoryOPPThree.js
@@ -0,0 +1,12 @@
+console.group("Memory STACK Javascript");
+// > 🎳 Las variables son referencias a un espacio en memoria.
+// > 🎩 Los navegadores web usan dos tipos de memorias: Stack y Heap.
+// > 📁 La memoria Stack es muy rápida, pero sin tanto espacio. Aquí se guardan los valores primitivos (booleanos, strings, números…).
+// > 🌪 La memoria Heap es más lenta, pero permite guardar enormes cantidades de información (son como los tornados: grandes, lentos y desordenados). En esta memoria guardamos los valores de los objetos ({...}).
+// > Entender cómo funciona la memoria en JavaScript no solo será útil para aprender POO, sino también para programación funcional. 😉
+
+// ============== MEMORIA ===================
+// > STACK > Nombres / Variables => Js (memoria STACK).
+// > HEAP > Objetos.
+// > Event Loop >
+console.groupEnd();
