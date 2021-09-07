b93eed23686943f10d46daa51ae9b594442e8149
Christiano0407 <placebo0704@gmail.com>
2021-09-07 10:00:01 -0500

    recursivos

--- intermedioOPP/oPPRecursividadFive.js
+++ intermedioOPP/oPPRecursividadFive.js
@@ -1,3 +1,9 @@
 console.group("Recursividad");
+// >> JSON parse y Strinify >>> No saben trabajr con funcions ==> Por eso la Recursividad.
+// >>>> Recursividad = Una función se llama así misma.
+// - Puedes llamar a esa misma función (recursiva), pero, ejecutar diferentes resultados.
 
+// ==================== Recursividad >>>>
+
+// ==================== Llamar >>
 console.groupEnd();
