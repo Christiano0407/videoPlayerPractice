66240d49264cefb6e22da0134f55b366823978fd
Christiano0407 <placebo0704@gmail.com>
2021-09-07 16:13:52 -0500

    finished-particles

--- canvas/appCanvasTwo.js
+++ canvas/appCanvasTwo.js
@@ -7,6 +7,7 @@ console.log(ctx);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const particleArray = [];
+let hue = 0;
 //>
 // Reescalar cada vez que, se vuelve responsive >
 window.addEventListener(`resize`, function () {
@@ -69,6 +70,7 @@ class Particle {
     this.size = Math.random() * 10 + 1;
     this.speedX = Math.random() * 1.5 - 1.5;
     this.speedY = Math.random() * 1.5 - 1.5;
+    this.color = `hsl( ` + hue + `, 100%, 50%)`; // Transformar el color>
   }
   update() {
     this.x += this.speedX;
@@ -77,9 +79,9 @@ class Particle {
     if (this.size > 0.2) this.size -= 0.1;
   }
   draw() {
-    ctx.fillStyle = "#ffffff";
+    ctx.fillStyle = this.color;
     //ctx.strokeStyle = "#ffffff";
-    ctx.lineWidth = 5;
+    //ctx.lineWidth = 5;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
@@ -102,6 +104,22 @@ function handleParticles() {
     particleArray[i].update();
     particleArray[i].draw();
 
+    // distance between i / j / and hipotemusa>
+    for (let j = i; j < particleArray.length; j++) {
+      const dx = particleArray[i].x - particleArray[j].x;
+      const dy = particleArray[i].y - particleArray[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < 100) {
+        ctx.beginPath();
+        ctx.strokeStyle = particleArray[i].color;
+        ctx.lineWidth = particleArray[i].size / 5;
+        ctx.moveTo(particleArray[i].x, particleArray[i].y);
+        ctx.lineTo(particleArray[j].x, particleArray[j].y);
+        ctx.stroke();
+        ctx.closePath();
+      }
+    }
     if (particleArray[i].size <= 0.3) {
       particleArray.splice(i, 1); //delete / replace => splice.
       //console.log(particleArray.length);
@@ -113,8 +131,11 @@ function handleParticles() {
 // ==> función animación >>
 animate = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //ctx.fillStyle = `rgba(0,0,0,0.02)`;
+  //ctx.fillRect(0, 0, canvas.width, canvas.height);
   //drawCircle();
   handleParticles();
+  hue += 2;
   requestAnimationFrame(animate);
 };
 animate();
